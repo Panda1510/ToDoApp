6ebe39c1669ddc803e10f30ea5b358d2f993ebf7
Anuj Yadav <arkyyadav001@gmail.com>
2021-05-15 00:35:04 +0530

    designed modal and edit and delete buttons

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import firebase from "firebase";
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+  const [deadLine, setDeadLine] = useState("");
 
   //when the app loads, we need to listen to the database and fetch new todos as they get added/removed
   //Runs once when the app loads
@@ -31,6 +32,7 @@ function App() {
             snapshot.docs.map((doc) => ({
               id: doc.id,
               todo: doc.data().todo,
+              deadLine: doc.data().deadLine,
             }))
           );
         });
@@ -43,40 +45,99 @@ function App() {
     db.collection("todos").add({
       todo: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      deadLine: deadLine,
     });
     setInput("");
+    setDeadLine('');
   };
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <form>
-        <FormControl>
-          <InputLabel>Write a task</InputLabel>
-          <Input
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-          />
-        </FormControl>
+    <Container>
+      <div className="App">
+        <h2>Manage you tasks here</h2>
+        <FormStyle>
+          <TakeTask>
+            <FormControl>
+              <InputLabel>Write a task</InputLabel>
+              <Input
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
+              />
+            </FormControl>
+          </TakeTask>
+          <TakeDeadLine>
+            <FormControl>
+              <InputLabel>Write Deadline</InputLabel>
+              <Input
+                value={deadLine}
+                onChange={(event) => setDeadLine(event.target.value)}
+              />
+            </FormControl>
+          </TakeDeadLine>
+          <SubmitTodo>
+            <Button
+              disabled={!input || !deadLine}
+              variant="contained"
+              color="primary"
+              type="submit"
+              onClick={addTodo}
+            >
+              Add Task
+            </Button>
+          </SubmitTodo>
+        </FormStyle>
 
-        <Button
-          disabled={!input}
-          variant="contained"
-          color="primary"
-          type="submit"
-          onClick={addTodo}
-        >
-          Add todo
-        </Button>
-      </form>
-
-      <ul>
-        {todos.map((todo) => (
-          <Todo todo={todo}></Todo>
-        ))}
-      </ul>
-    </div>
+        <MyList>
+          {todos.map((todo) => (
+            <Todo todo={todo}></Todo>
+          ))}
+        </MyList>
+      </div>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  border-radius: 10px;
+  background-color: white;
+  height: 90%;
+  width: 50%;
+  margin: 30px auto;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+  text-align: center;
+  h2 {
+    color: rgba(0, 0, 0, 0.5);
+    padding-top: 15px;
+    margin-top: 20%;
+  }
+`;
+
+const TakeTask = styled.div`
+  width: 100%;
+  margin-top: 15px;
+`;
+
+const TakeDeadLine = styled.div`
+  width: 100%;
+  margin-top: 15px;
+`;
+
+const SubmitTodo = styled.div`
+  width: 100%;
+  margin-top: 25px;
+  margin-bottom: 20px;
+`;
+
+const FormStyle = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
+const MyList = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+`;
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,11 +9,15 @@ import {
   ListItemAvatar,
   ListItemText,
   ImageIcon,
+  InputLabel,
+  Input,
+  FormControl,
 } from "@material-ui/core";
 import "./Todo.css";
 import db from "../firebase";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -30,12 +34,14 @@ function Todo(props) {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const [input, setInput] = useState("");
+  const [deadLine, setDeadLine] = useState("");
 
   const updateTodo = () => {
     //update the todo with the new input text
     db.collection("todos").doc(props.todo.id).set(
       {
         todo: input,
+        deadLine: deadLine,
       },
       { merge: true }
     );
@@ -50,33 +56,77 @@ function Todo(props) {
         onClose={(e) => {
           setOpen(false);
         }}
+        className="modal"
       >
-        <div className={classes.paper}>
-          <h1>I am a model</h1>
-          <input
-            placeholder = {props.todo.todo}
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-          />
-          <Button onClick={updateTodo}>Update</Button>
-        </div>
+        <ModalContainer>
+          <div className="modal-content">
+            <h2 className="modal-heading">Update Your Task here</h2>
+            <TakeTask>
+              <FormControl>
+                <InputLabel>Enter your updated task:</InputLabel>
+                <Input
+                  placeholder={props.todo.todo}
+                  value={input}
+                  onChange={(event) => setInput(event.target.value)}
+                  className="modal-input"
+                />
+              </FormControl>
+            </TakeTask>
+            <TakeDeadLine>
+              <FormControl>
+                <InputLabel>Update Deadline:</InputLabel>
+                <Input
+                  placeholder={props.todo.deadLine}
+                  value={deadLine}
+                  onChange={(event) => setDeadLine(event.target.value)}
+                  className="modal-input"
+                />
+              </FormControl>
+            </TakeDeadLine>
+            <Button onClick={updateTodo}>Update</Button>
+          </div>
+        </ModalContainer>
       </Modal>
       <List className="todo__list">
-        <ListItem>
+        <ListItem className="todo__list-item">
           <ListItemAvatar>
             <Avatar></Avatar>
           </ListItemAvatar>
-          <ListItemText primary={props.todo.todo} secondary="dummy deadline" />
+          <ListItemText
+            primary={props.todo.todo}
+            secondary={props.todo.deadLine}
+          />
+
+          <Button onClick={(e) => setOpen(true)} className = 'button-edit'>Edit</Button>
+          <Button
+            onClick={(event) =>
+              db.collection("todos").doc(props.todo.id).delete()
+            }
+            className = 'button-delete'
+          >
+            <DeleteForeverIcon />
+          </Button>
         </ListItem>
-        <button onClick={(e) => setOpen(true)}>Edit</button>
-        <DeleteForeverIcon
-          onClick={(event) =>
-            db.collection("todos").doc(props.todo.id).delete()
-          }
-        />
       </List>
     </>
   );
 }
+
+const ModalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 90px;
+`;
+
+const TakeTask = styled.div`
+  width: 100%;
+  margin-top: 15px;
+`;
+
+const TakeDeadLine = styled.div`
+  width: 100%;
+  margin-top: 15px;
+`;
 
 export default Todo;
